1af6a0176713209b9db6da0b94abf6260965dc9c
pradeepkrishnamurthy <pradeeepmurthy@gmail.com>
2025-08-04 12:01:33 +0530

    Spring boot camel

---
+++ src/main/java/com/myapp/camel/CamelSpringBootIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelSpringBootIntegrationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelSpringBootIntegrationDemoApplication.class, args);
+    }
+
+}

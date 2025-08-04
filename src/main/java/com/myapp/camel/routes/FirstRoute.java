16f0a10d019e40cd395770d1b197cd20ff929700
pradeepkrishnamurthy <pradeeepmurthy@gmail.com>
2025-08-04 12:40:46 +0530

    xml dsl Route Demo

--- src/main/java/com/myapp/camel/routes/FirstRoute.java
+++ src/main/java/com/myapp/camel/routes/FirstRoute.java
@@ -3,7 +3,7 @@ package com.myapp.camel.routes;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class FirstRoute extends RouteBuilder {
     public void configure() throws Exception {
         from("file:data/input?noop=true&recursive=true&initialDelay=5000&delay=2000&include=.*\\.txt$")
@@ -11,3 +11,9 @@ public class FirstRoute extends RouteBuilder {
                 .to("file:data/output?noop=true");
     }
 }
+
+// Earlier versions camel dsl were written using xml
+
+// CamelContext
+// Configure the routes
+// start the routes manually

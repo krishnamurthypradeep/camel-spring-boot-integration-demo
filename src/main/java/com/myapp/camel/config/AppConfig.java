1454e986fc6b64e2770261403cca6d1296be843f
pradeepkrishnamurthy <pradeeepmurthy@gmail.com>
2025-08-05 10:27:05 +0530

    file to active mq convert xml to json

---
+++ src/main/java/com/myapp/camel/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.myapp.camel.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean("activeMQ")
+    ActiveMQConnectionFactory connectionFactory (){
+     return new ActiveMQConnectionFactory("tcp://localhost:61616");
+    }
+
+}
